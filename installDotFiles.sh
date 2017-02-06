2931a0c08a210f0d08cd4630c7aff23dcdf02fd0
sundar <tmsundarapandian@gmail.com>
2017-02-06 08:58:08 +0530

    Added terminator

--- installDotFiles.sh
+++ installDotFiles.sh
@@ -3,7 +3,8 @@ ln -s $PWD/bashrc_sundar ~/.bashrc
 ln -s $PWD/vimrc  ~/.vimrc
 ln -s $PWD/screenrc  ~/.screenrc
 ln -s $PWD/tmux.conf  ~/.tmux.conf
-ln -s $PWD/openbox  ~/.config/openbox
+ln -s $PWD/openbox  ~/.config/
+ln -s $PWD/terminator  ~/.config/
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 mkdir -p ~/.emacs.d
 ln -s $PWD/init.el  ~/.emacs.d/init.el
